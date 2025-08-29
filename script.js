059208ad8dd20eb6a4ac081147b1944582798d63
Constantine Elias Macris <c.macris@acg.edu>
2025-08-29 00:14:01 -0500

    add code

---
+++ script.js
@@ -0,0 +1,22 @@
+let price = 1.87;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
+
+const cashInput = document.getElementById("cash");
+
+function purchaseClick() {
+  const cashInCents = Math.round(Number(cash.value) * 100);
+  const priceInCents = Math.round(price * 100);
+  if (cashInCents < priceInCents) {
+    alert("Customer does not have enough money to purchase the item.");
+  }
+}
