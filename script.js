85f7172e2b2293ccb4e42fa7351ea5157837868e
Constantine Elias Macris <c.macris@acg.edu>
2025-11-08 18:41:45 -0600

    add code

--- script.js
+++ script.js
@@ -11,26 +11,42 @@ let cid = [
   ['ONE HUNDRED', 100]
 ];
 
-const cashInput = document.getElementById("cash");
-const changeDueDiv = document.getElementById('change-due');
-const reversedCid = [...cid]
-    .reverse()
-    .map(([denominationName, amount]) => [
-      denominationName,
-      Math.round(amount * 100)
-    ]);
-const denominations = [10000, 2000, 1000, 500, 100, 25, 10, 5, 1];
-const result = { status: 'OPEN', change: [] };
-const totalCID = reversedCid.reduce((prev, [_, amount]) => prev + amount, 0);
+let cashInput;
+let changeDueDiv
+
+window.onload = () => {
+  cashInput = document.getElementById("cash");
+  changeDueDiv = document.getElementById('change-due');
+
+};
+
+
 
 
 function purchaseClick() {
-  console.log(cashInCents, priceInCents);
-  const cashInCents = Math.round(Number(cash.value) * 100);
+  const cashInCents = Math.round(Number(cashInput.value) * 100);
   const priceInCents = Math.round(price * 100);
   if (cashInCents < priceInCents) {
     alert("Customer does not have enough money to purchase the item.");
   } else if (cashInCents === priceInCents) {
     changeDueDiv.innerHTML = "No change due - customer paid with exact cash.";
+  } else {
+    let changeDue = cashInCents - priceInCents;
+    console.log(cashInCents, priceInCents, totalCID);
+    const reversedCid = [...cid]
+        .reverse()
+        .map(([denominationName, amount]) => [
+          denominationName,
+          Math.round(amount * 100)
+        ]);
+    const denominations = [10000, 2000, 1000, 500, 100, 25, 10, 5, 1];
+    const result = { status: 'OPEN', change: [] };
+    const totalCID = reversedCid.reduce((prev, [_, amount]) => prev + amount, 0);
+    if (changeDue > totalCID) {
+      changeDueDiv.innerHTML = "<p>STATUS: INSUFFICIENT_FUNDS</p>";
+    }
+    if (changeDue === totalCID) {
+      changeDueDiv.innerHTML = "<p>STATUS: CLOSED</p>";
+    }
   }
 }
